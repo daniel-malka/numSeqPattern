77c55211fd6d616daec77a34b7ae75c3f1a3c711
Daniel <daniel847@tutanota.com>
2023-03-02 23:01:07 +0200

    feat:complexity+rectORellipse+jsoncolorTable

--- sketch2.js
+++ sketch2.js
@@ -2,7 +2,7 @@
 sequenceJump = [];
 
 //this num gets ++ until <= sequenceJump.length and comes
-//back to = 0 using ternary % (the function is in line 58);
+//back to = 0 using ternary % (the function is in line 94);
 arrayLooper = 0;
 //this value gets incremented by the number in the array sequenceJump[arrayLooper];
 incAndJump = 0;
@@ -27,30 +27,66 @@ function setup() {
   **/
   palettesArray = Object.values(palettes);
   createCanvas(windowWidth, windowHeight);
-  background(0);
+  palette1 = floor(random(palettesArray.length));
+  fill(
+    palettesArray[palette1].rgb[0],
+    palettesArray[palette1].rgb[1],
+    palettesArray[palette1].rgb[2]
+  );
   noStroke();
+  rect(0, 0, windowWidth / 2.2);
+  palette2 = floor(random(palettesArray.length));
+  fill(
+    palettesArray[palette2].rgb[0],
+    palettesArray[palette2].rgb[1],
+    palettesArray[palette2].rgb[2]
+  );
+  rect(windowWidth / 2.2, 0, windowWidth / 1.8, height);
+  palette3 = floor(random(palettesArray.length));
+  fill(
+    palettesArray[palette3].rgb[0],
+    palettesArray[palette3].rgb[1],
+    palettesArray[palette3].rgb[2]
+  );
+  rect(0, windowHeight - windowWidth / 8.23, width, 233);
 }
 
 function draw() {
   noLoop();
   noStroke();
   // noiseSeed(1);
-  sequenceJump = [1, 4, 1, 1, 22];
+  sequenceJump = [1, 2, 4, 1, 2, 4, 3];
 
-  for (y = 0; y <= height; y += 25) {
-    for (x = 0; x <= width; x += 25) {
-      //increment this value by the number inside insequence[jump] location
+  for (y = 50; y <= height - 50; y += 100) {
+    for (x = 50; x <= width; x += 100) {
+      // thios if statement checks if its the right time to do somthing acording to  sequence array
       if (i == incAndJump) {
-        palette1 = floor(random(palettesArray.length));
-        fill(
-          palettesArray[palette1].rgb[0],
-          palettesArray[palette1].rgb[1],
-          palettesArray[palette1].rgb[2]
-        );
-        ellipse(x, y, 65, 65);
-        if (sequenceJump[arrayLooper] > 10) {
+        //random color pallete from the pre loaded json file
+
+        if (random(10) < 5) {
+          palette4 = floor(random(palettesArray.length));
+          fill(
+            palettesArray[palette4].rgb[0],
+            palettesArray[palette4].rgb[1],
+            palettesArray[palette4].rgb[2]
+          );
+          if (random(1) > 0.5) {
+            rect(x, y - 50, 100, 100);
+          } else {
+            rect(x, y, 100, 100);
+          }
         } else {
-          ellipse(x, y, 50, 50);
+          palette5 = floor(random(palettesArray.length));
+          fill(
+            palettesArray[palette5].rgb[0],
+            palettesArray[palette5].rgb[1],
+            palettesArray[palette5].rgb[2]
+          );
+          if (random(1) > 0.5) {
+            ellipse(x, y, 100, 100);
+          } else {
+            ellipse(x + 25, y, 100, 100);
+          }
         }
         fill(255, random(256));
 
